517ffdd65eba4c8224c6f156ad083cd18b8f0973
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-01-14 14:47:31 +0100

    fixing the pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """ a fabric script that generates a .tgz archive from the web_static content
     folder in the AirBnB_clone repo, using the function do_pack"""
-from fabric.api  import local
+from fabric.api import local
 from datetime import datetime
+
 
 def do_pack():
     """a function that creates an archive for a directory
 
     Returns:
-        tgz file: the archive path if the archive has been correctly generated 
+        tgz file: the archive path if the archive has been correctly generated
     """
     try:
         current_time = datetime.now().strftime("%Y%m%d%H%M%S")
         archive_path = "versions/web_static_{}.tgz".format(current_time)
-        #create the folder versions if not exists
+        # create the folder versions if not exists
         local("mkdir -p versions")
-        #create the archive
+        # create the archive
         local("tar -czvf {} web_static".format(archive_path))
         return archive_path
-    except:
+    except Exception:
         return None
-
