cc893544dc0e4d402b1858f362c017f4a36b8317
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-01-15 19:36:06 +0100

    fixing pycoestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ import os.path
 # Connecting to the servers
 env.hosts = ['54.146.92.28']
 
+
 def do_deploy(archive_path):
     """A function that deploys files to servers
     """
@@ -16,23 +17,26 @@ def do_deploy(archive_path):
     try:
         # Uploading the ARCHIVE to the remote servers at /tmp/
         put(archive_path, '/tmp/')
-        
+
         # Extracting the file name only without tzg extension
         archive_name = archive_path.split('/')[-1]  # includes .tgz
         file_name = archive_name.split('.')[0]  # without .tgz
-        
+
         # Creating the directory to store the extracted contents
         path_to_create = f"/data/web_static/releases/{file_name}"
         run(f'sudo mkdir -p {path_to_create}')
-        
+
         # Uncompress the archive into the directory created above
-        run(f"sudo tar -xzf /tmp/{archive_name} -C {path_to_create} --strip-components=1")
+        run(f"sudo tar -xzf /tmp/{archive_name} "
+            f"-C {path_to_create} "
+            f"--strip-components=1")
+
         # Remove the archive from the web server
         run(f"sudo rm /tmp/{archive_name}")
-        
+
         # Deleting the current symbolic link from the servers
         run(f"sudo rm -f /data/web_static/current")
-        
+
         # Creating a new symbolic link pointing to the extracted contents
         run(f"sudo ln -s {path_to_create} /data/web_static/current")
 
