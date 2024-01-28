3d0747421252d0f85754e4ebf2c6bae20ba0445c
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-01-28 16:55:37 +0100

    routing to /hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" A simple Flask app """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Display "Hello HBNB!" """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Display "HBNB" """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="192.168.100.115", port=5000)
