36116e8c9914b79a2b7ba327865fb4fe19414ec8
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-01-29 01:33:42 +0100

    rendering an html file

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" A simple Flask app """
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Display "Hello HBNB!" """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Display "HBNB" """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_root(text):
+    """making a dynamic URL by taking a variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>/", strict_slashes=False)
+def python(text="is cool"):
+    """displays python + text """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>",  strict_slashes=False)
+def number(n):
+    """displays n is anulber if int is 1"""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', num=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
